9f61175816d63dd01e64a39c938cc1f46d4adfb5
Olav Magnus Nevestad Aggvin <36802756+omaggvin@users.noreply.github.com>
2024-11-06 14:44:14 +1100

    test

---
+++ turtlebot/ertg/launch/ertg.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+
+        # customize launch file below
+
+        # end launch file customization
+    ])
